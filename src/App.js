3e19e12492d4d31c06dffea8b01486ce20690070
hemaddlawar <hema.200219627512@gmail.com>
2023-12-22 21:46:56 +0300

    install bulma css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import "./App.css";
+import MoviePlaylist from "./components/MoviePlaylist";
+import SongPlaylist from "./components/SongPlaylist";
 
-import './App.css';
+export default function App() {
+  const handleResetClick = () => {
+    //
+  };
 
-function App() {
   return (
-    <div >
-      lksdjflk
+    <div className="container is-fluid">
+      <button onClick={() => handleResetClick()} className="button is-danger">
+        Reset Both Playlists
+      </button>
+      <hr />
+      <MoviePlaylist />
+      <hr />
+      <SongPlaylist />
     </div>
   );
 }
-
-export default App;
