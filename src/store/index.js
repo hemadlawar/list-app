ca81731985691359c2cc292f0f649fc81ef68f77
hemaddlawar <hema.200219627512@gmail.com>
2023-12-24 21:36:07 +0300

    add slice and store of movie lists

--- src/store/index.js
+++ src/store/index.js
@@ -14,9 +14,24 @@ const songsSlice = createSlice({
   },
 });
 
+const movieSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      console.log(`action.payload = ${action.payload}`);
+      state.splice(action.payload, 1); // returns correct id
+    },
+  },
+});
+
 const store = configureStore({
   reducer: {
     songs: songsSlice.reducer,
+    movies: movieSlice.reducer,
   },
 });
 export { store };
